7e92c8d1367a7e0646cd28db3d0534540d5684ab
Jesus Obispo <jesus.obispo@educa.madrid.org>
2026-03-23 11:56:17 +0100

    datos_textil listo

---
+++ assets/js/datos_textil.js
@@ -0,0 +1,17 @@
+const productosB = [
+  { id: 1, nombre: "Camiseta", precio: 20, stock: 50, categoria: "ropa" },
+  { id: 2, nombre: "Pantalon", precio: 35, stock: 30, categoria: "ropa" }
+];
+
+// descuento
+function aplicarDescuento(porcentaje) {
+  return productosB.map(p => {
+    p.precio = p.precio - (p.precio * porcentaje / 100);
+    return p;
+  });
+}
+
+// código seguridad
+function codigo(nombre) {
+  return nombre.split("").reverse().join("");
+}
